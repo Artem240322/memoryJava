0d969211e94068b9b4afa8a6547cf650fe6f4f42
ArtemCH240322 <sun2383@mail.ru>
2022-05-25 13:23:44 +0300

    Первый комит к первой ДР второго курса. Задача 5. Задача 6.

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,5 +1,7 @@
 package ru.skypro;
 
+import java.util.Arrays;
+
 public class Main {
     // задача 5 (1)
     public static void main(String[] args) {
@@ -13,18 +15,35 @@ public class Main {
 
     }
 
-}
+
     // задача 6 (2)
-       public static void main(String[] args) {
-          Integer value = 33;
-          changeValue(value);
-          System.out.println(value);
+    public static void String(String[] args) {
+        Integer value = 33;
+        changeValue(value);
+        System.out.println(value);
     }
 
-          private static void changeValue(Integer value) {
-          value = 22;
-          System.out.println(value);
+    private static void changeValue(Integer value) {
+        value = 22;
+        System.out.println(value);
     }
+
+
+
+    /// Задача 7 (3)
+    public static void Vaule(String[] args) {
+        Integer[] value = {3, 4};
+        changeValue(value.length);
+        System.out.println(Arrays.toString(new Integer[]{value.length}));
+    }
+    private static void Integer(Integer value) {
+        value[0] = 99;
+    }
+
+
+    /// Задача 8 (4)
+
 }
+
 
 
