900aada8be33512e7731ae69b815302dc63e5448
ArtemCH240322 <sun2383@mail.ru>
2022-05-25 13:01:51 +0300

    Первый комит к первой ДР второго курса. Задача 5. Задача 6

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -1,14 +1,30 @@
 package ru.skypro;
 
 public class Main {
-
+    // задача 5 (1)
     public static void main(String[] args) {
         int value = 33;
-        changeValue (value);
+        changeValue(value);
         System.out.println(value);
     }
+
     private static void changeValue(int value) {
         value = 22;
 
     }
+
 }
+    // задача 6 (2)
+       public static void main(String[] args) {
+          Integer value = 33;
+          changeValue(value);
+          System.out.println(value);
+    }
+
+          private static void changeValue(Integer value) {
+          value = 22;
+          System.out.println(value);
+    }
+}
+
+
