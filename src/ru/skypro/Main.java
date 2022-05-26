14f46e4b79359ec9c70770aa0ee5ec397c2e632c
ArtemCH240322 <sun2383@mail.ru>
2022-05-26 13:40:28 +0300

    Первый комит к первой ДР второго курса. Задача 5. Задача 6. Задача 7. Задача 8. Задача 9 и 10 правки. еще правки (, и еще правки/ ,kby

--- src/ru/skypro/Main.java
+++ src/ru/skypro/Main.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class Main {
     // задача 5 (1)
-   public static void main(String[] args) {
+   public static void main(String args) {
         int value = 33;
         changeValue(value);
         System.out.println(value);
@@ -58,7 +58,7 @@ public class Main {
     /// Задача 9-10
 
     public static void main(String[] args) {
-        Person person = new Person("Alla", "Pugacheva");
+        Person person = new Person("Alla","Pugacheva");
         chargeValue(person);
         System.out.println(person);
     }
